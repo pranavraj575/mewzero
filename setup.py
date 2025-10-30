362d80a238f045e6a829980ba1e92c3bc4b732a5
pranavraj575 <40303088+pranavraj575@users.noreply.github.com>
2025-10-30 12:03:50 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='mewzero',
+    version='6.9.0',
+    packages=find_packages(),
+    install_requires=['numpy',
+                      ],
+    license='Liscence to Krill',
+)
